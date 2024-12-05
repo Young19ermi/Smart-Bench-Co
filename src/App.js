cb13b82ef6655b5728129222c490d43516801f7b
Ermias Seleshi Endale <111576904+Young19ermi@users.noreply.github.com>
2024-12-04 17:32:13 -0800

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './index.css';
+import Pay from './Pages/Pay';
+
+function App() {
+  return (
+    <div>
+      <Pay/>
+    </div>
+  );
+}
+
+export default App;
